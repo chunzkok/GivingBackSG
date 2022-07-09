e8371d94f48895b409c1fd02de1cbabe3aee4e3c
chunzkok <kokcz@comp.nus.edu.sg>
2022-07-10 02:10:26 +0800

    Added logic to update AuthContext on relevant changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,88 @@
 import "./App.css";
+import React, { useEffect, useContext } from "react";
+import AuthContext from "./utils/AuthContext";
+import supabase from "./config/supabase";
 import Routes from "./Routes";
 
 function App() {
+  const { authData, setAuthData, setAuthLoading } = useContext(AuthContext);
+
+  const parseProfile = (profileData) => {
+    const { id, avatar_url, name, telegram, phone } = profileData;
+    return {
+      logged_in: true,
+      id,
+      name,
+      telegram,
+      phone,
+      avatar_url: avatar_url
+        ? supabase.storage.from("avatars").getPublicUrl(avatar_url).publicURL
+        : "/images/avatar_default.png",
+    };
+  };
+  // Initialise authData and setup listeners, only done once at the start.
+  useEffect(() => {
+    const getAccountData = async (user_id) => {
+      try {
+        const { data, error } = await supabase
+          .from("profiles")
+          .select("*")
+          .eq("id", user_id)
+          .single();
+        if (error) throw error;
+        return data;
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+
+    // Fetch initial user state
+    if (supabase.auth.user()) {
+      (async () => {
+        setAuthLoading(true);
+        const accountData = await getAccountData(supabase.auth.user().id);
+        setAuthData(parseProfile(accountData));
+        setAuthLoading(false);
+      })();
+    }
+
+    const parseSession = async (session) => {
+      if (!session)
+        return {
+          logged_in: false,
+          permissions: 0,
+          username: null,
+          avatar_url: null,
+          id: null,
+        };
+
+      const { user } = session;
+
+      return parseProfile(await getAccountData(user?.id));
+    };
+
+    supabase.auth.onAuthStateChange(async (_, session) => {
+      setAuthLoading(true);
+      setAuthData(await parseSession(session));
+      setAuthLoading(false);
+    });
+  }, [setAuthData, setAuthLoading]);
+
+  // Also set up listener for profile changes
+  useEffect(() => {
+    const uid = authData.id;
+    if (!uid) return;
+    const profileSub = supabase
+      .from(`profiles:id=eq.${uid}`)
+      .on("UPDATE", (payload) => {
+        setAuthLoading(true);
+        setAuthData(parseProfile(payload.new));
+        setAuthLoading(false);
+      })
+      .subscribe();
+    return () => supabase.removeSubscription(profileSub);
+  }, [authData, setAuthData, setAuthLoading]);
+
   return (
     <div className="App">
       <Routes />
